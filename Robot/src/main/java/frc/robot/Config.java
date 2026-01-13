095af3d6d6133a6bd5f34c7ae7b79df0e0916a1d
Vincent <vincenttueminh@gmail.com>
2026-01-13 14:49:28 -0500

    mesure unit conversion

--- Robot/src/main/java/frc/robot/Config.java
+++ Robot/src/main/java/frc/robot/Config.java
@@ -2,7 +2,7 @@ package frc.robot;
 
 // FIXME: Est ce que c'est bon comme ça ou on ferait une instance de la classe ?
 public class Config {
-    public static double robot_width = 100; // donnée hypotétique, en cm
-    public static double robot_lenght = 100; // donnée hypotétique, en cm
-    public static double robot_height = 50; // donnée hypotétique, en cm
+    public static double robot_width = 1; // donnée hypotétique, en m
+    public static double robot_lenght = 1; // donnée hypotétique, en m
+    public static double robot_height = 0.5; // donnée hypotétique, en m
 }
