93d583ebec5820c1a3d55543da50bb3f7a7b6526
Robots Montagnards <robotsmontagnards@gmail.com>
2026-03-09 12:53:30 -0400

    launcher controller setup

--- Robot/src/main/java/frc/robot/Config.java
+++ Robot/src/main/java/frc/robot/Config.java
@@ -2,7 +2,5 @@ package frc.robot;
 
 // FIXME: Est ce que c'est bon comme ça ou on ferait une instance de la classe ?
 public class Config {
-    public static double robot_width = 1; // donnée hypotétique, en m
-    public static double robot_lenght = 1; // donnée hypotétique, en m
-    public static double robot_height = 0.5; // donnée hypotétique, en m
+    public static double bumper_sensitivity = 0.5; // 0 is not pressed and 1 is fully pressed
 }
--- Robot/src/main/java/frc/robot/components/Controller.java
+++ Robot/src/main/java/frc/robot/components/Controller.java
@@ -3,11 +3,17 @@ package frc.robot.components;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.data.StickPosition;
+import frc.robot.Config;
 
 public class Controller {
     // TODO: mettre les bonnes affaires ici, une fois la manette décidé et accepté par l'équipe
     final static CommandXboxController xbox_controller = new CommandXboxController(0);
-    Controller() {}
+
+    private double bumper_sensitivity;
+
+    Controller() {
+        bumper_sensitivity = Config.bumper_sensitivity;
+    }
 
     public static CommandXboxController getController() {
         return xbox_controller;
@@ -19,6 +25,14 @@ public class Controller {
 
     public StickPosition getRightStickPosition() {
         return new StickPosition( xbox_controller.getRightX(), xbox_controller.getRightY() );
+    }
+
+    public boolean getLauncherToggle() {
+        double right_bumper_state = xbox_controller.getRightTriggerAxis();
+        if( right_bumper_state > bumper_sensitivity ) {
+            return true;
+        }
+        return false;
     }
 }
  
