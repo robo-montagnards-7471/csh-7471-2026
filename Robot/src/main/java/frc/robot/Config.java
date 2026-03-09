dcd312a61210b954c9a44829169e165023e31438
Robots Montagnards <robotsmontagnards@gmail.com>
2026-03-09 13:31:20 -0400

    just variables renaming

--- Robot/src/main/java/frc/robot/Config.java
+++ Robot/src/main/java/frc/robot/Config.java
@@ -2,14 +2,14 @@ package frc.robot;
 
 public class Config {
 
-    // CAN BUS
+    // CAN BUS port
     // Swerve
-    public static double front_left_port_drive  = 1;
-    public static double front_left_port_steer  = 5;
-    public static double front_right_port_drive = 2;
-    public static double front_right_port_steer = 6;
-    public static double back_left_port_drive   = 10;
-    public static double back_left_port_steer   = 7;
-    public static double back_right_port_drive  = 11;
-    public static double back_right_port_steer  = 8;
+    public static double front_left_drive  = 1;
+    public static double front_left_steer  = 5;
+    public static double front_right_drive = 2;
+    public static double front_right_steer = 6;
+    public static double back_left_drive   = 10;
+    public static double back_left_steer   = 7;
+    public static double back_right_drive  = 11;
+    public static double back_right_steer  = 8;
 }
