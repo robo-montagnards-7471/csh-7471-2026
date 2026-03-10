32e474c64cbb4cd1200dfb3267c386dab7d5a56e
Vincent <vincenttueminh@gmail.com>
2026-03-10 17:31:11 -0400

    config launcher

--- Robot/src/main/java/frc/robot/Config.java
+++ Robot/src/main/java/frc/robot/Config.java
@@ -6,6 +6,7 @@ public class Config {
     public static double bumper_sensitivity = 0.5; // 0 is not pressed and 1 is fully pressed
 
     // Motors configuration
+    // Launcher
     public static double launcher_output_power = 1; // 1 is full speed, -1 is full speed other direction
     public static double launcher_input_power = 0.75; // 1 is full speed, -1 is full speed other direction
 
