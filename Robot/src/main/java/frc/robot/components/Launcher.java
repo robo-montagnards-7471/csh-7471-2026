330444a47d7dfefdffc18a625b83de4230570aad
Vincent <vincenttueminh@gmail.com>
2026-03-10 19:59:42 -0400

    PID (to test)

--- Robot/src/main/java/frc/robot/components/Launcher.java
+++ Robot/src/main/java/frc/robot/components/Launcher.java
@@ -1,10 +1,15 @@
 package frc.robot.components;
 
 import com.revrobotics.spark.SparkMax;
+
+import javax.print.attribute.standard.Destination;
+
 // import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel;
 import frc.robot.Config;
+
+import edu.wpi.first.math.controller.PIDController;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -16,6 +21,9 @@ public class Launcher {
     private SparkFlex output_motor_follower;
     private SparkMax input_motor;
 
+    private PIDController pid_leader_controller;
+    private PIDController pid_follower_controller;
+
     public Launcher() {
         is_output_active = true;
         is_input_active = false;
@@ -26,6 +34,17 @@ public class Launcher {
         SmartDashboard.putNumber( "Launcher Leader", output_motor_leader.get() );
         SmartDashboard.putNumber("Launcher Follower", output_motor_follower.get());
         SmartDashboard.putNumber( "Launcher Input", input_motor.get() );
+
+        pid_leader_controller = new PIDController(1.09, 1.02, 0.06);
+        pid_follower_controller = new PIDController(1.09, 1.02, 0.06);
+    }
+
+    private void setOutputMotors( double destination ) {
+        double output_leader_voltage = pid_leader_controller.calculate( output_motor_leader.get(), destination);
+        double output_follower_voltage = pid_leader_controller.calculate( output_motor_follower.get(), -destination);
+
+        output_motor_leader.set( output_leader_voltage );
+        output_motor_follower.set( output_follower_voltage );
     }
 
     public void poll( boolean toggle_input, boolean toggle_output ) {
@@ -38,12 +57,10 @@ public class Launcher {
 
         
         if( is_output_active ) {
-            output_motor_leader.set( Config.launcher_output_power );
-            // output_motor_follower.set( -Config.launcher_output_power );
+            setOutputMotors( Config.launcher_output_power );
         }
         else {
-            output_motor_leader.set( 0 );
-            // output_motor_follower.set( 0 );
+            setOutputMotors( 0 );
         }
 
         if( is_input_active ) {
