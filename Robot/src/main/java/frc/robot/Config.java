ce1b2e9d79d8f9e970f9df71ffb90cb813d6fee4
Robots Montagnards <robotsmontagnards@gmail.com>
2026-03-10 18:46:59 -0400

    climber

--- Robot/src/main/java/frc/robot/Config.java
+++ Robot/src/main/java/frc/robot/Config.java
@@ -2,7 +2,12 @@ package frc.robot;
 
 // FIXME: Est ce que c'est bon comme ça ou on ferait une instance de la classe ?
 public class Config {
-    public static double robot_width = 1; // donnée hypotétique, en m
-    public static double robot_lenght = 1; // donnée hypotétique, en m
-    public static double robot_height = 0.5; // donnée hypotétique, en m
+    // Motors configuration
+    // Climber
+    public static double climb_up = 1;
+    public static double climb_down = -1;
+
+    // CAN BUS port
+    // Climber
+    public static int climber = 15;
 }
---
+++ Robot/src/main/java/frc/robot/components/Climber.java
@@ -0,0 +1,47 @@
+package frc.robot.components;
+
+import com.revrobotics.spark.SparkMax;
+
+import javax.lang.model.util.ElementScanner14;
+
+import com.revrobotics.spark.SparkLowLevel;
+import frc.robot.Config;
+
+public class Climber {
+    private SparkMax motor;
+
+    private boolean is_up;
+    private boolean is_down;
+
+    public Climber() {
+        is_down = false;
+        is_up = false;
+
+        motor = new SparkMax( Config.climber, SparkLowLevel.MotorType.kBrushless );
+    }
+
+    public void poll( boolean climb_up, boolean climb_down ) {
+        if( climb_up ) {
+            is_up = true;
+            is_down = false;
+        }
+        else if( climb_down ) {
+            is_down = true;
+            is_up = false;
+        }
+        else {
+            is_up = false;
+            is_down = false;
+        }
+
+        if( is_up ) {
+            motor.set( Config.climb_up );
+        }
+        else if ( climb_down ) {
+            motor.set( Config.climb_down );
+        }
+        else {
+            motor.set( 0 );
+        }
+    }
+}
