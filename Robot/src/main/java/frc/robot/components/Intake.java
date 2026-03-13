2dc459445fa831a7bacec08cd25f94a596f78f94
Robots Montagnards <robotsmontagnards@gmail.com>
2026-03-13 17:56:42 -0400

    remove PID (and it now works)

--- Robot/src/main/java/frc/robot/components/Intake.java
+++ Robot/src/main/java/frc/robot/components/Intake.java
@@ -3,7 +3,6 @@ package frc.robot.components;
 import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.math.controller.PIDController;
 import com.revrobotics.spark.SparkLowLevel;
 import frc.robot.Config;
 
@@ -13,15 +12,12 @@ public class Intake {
 
     private SparkMax motor;
 
-    private PIDController intake_pid;
 
     public Intake() {
         is_in = false;
         is_out = false;
 
         motor = new SparkMax( Config.intake, SparkLowLevel.MotorType.kBrushless );
-
-        intake_pid = new PIDController(1.09, 1.02, 0.06);
 
         SmartDashboard.putNumber("Intake Speed", motor.get());
     }
@@ -37,13 +33,13 @@ public class Intake {
         }
 
         if( is_in ) {
-            motor.set( intake_pid.calculate( motor.get(), Config.in ) );
+            motor.set( Config.in );
         }
         else if( is_out ) {
-            motor.set( intake_pid.calculate( motor.get(), Config.out ) );
+            motor.set( Config.out );
         }
         else {
-            motor.set( intake_pid.calculate( motor.get(), 0 ) );
+            motor.set( 0 );
         }
         SmartDashboard.putNumber("Intake Speed", motor.get());
     }
