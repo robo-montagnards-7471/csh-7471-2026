565f11219c50f59220e937d7cd456d1d78cf9e87
Robots Montagnards <robotsmontagnards@gmail.com>
2026-03-25 18:34:54 -0400

    SendableChooser

--- Robot/src/main/java/frc/robot/Robot.java
+++ Robot/src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import frc.robot.components.Climber;
 import frc.robot.components.Intake;
 import frc.robot.components.Shooter;
